0255a41ae632a04daf4327219d9e324ba14acf44
Tom Smith <14467600@students.lincoln.ac.uk>
2018-04-12 16:58:31 +0100

    adult vs child data
    Implemented, need to do more specific comparisons and patterns

--- RWData.js
+++ RWData.js
@@ -105,8 +105,11 @@ function updateData()
 			else
 				LoadChar();
 		break;
+		case 6:
+			LoadResponses();
+		break;
 		default:
-			
+			LoadAge();
 		break;
 	}
 }
@@ -116,6 +119,10 @@ var irlTemp = [];
 var vgTemp = [];
 var tempPush1 = [];
 var tempPush2 = [];
+var responseMass = [];
+var responseOut = [];
+var responseCompare = [];
+
 
 var userIDs = [];
 var userAges = [];
@@ -164,6 +171,9 @@ function gotData(data)
 	userRoles = [];
 	userGames = [];
 	userChars = [];
+	responseMass = [];
+	responseOut = [];
+	responseCompare = [];
 	
 	characters = [];
 	games = [];
@@ -180,6 +190,8 @@ function gotData(data)
 	var users = data.val();
 	var keys = Object.keys(users);
 	//console.log(keys);
+	
+	//For each response
 	for(var i = 0; i < keys.length; i++)
 	{
 		//mfq[i] = "";
@@ -256,6 +268,7 @@ function gotData(data)
 			
 			
 			tempPush2 = [];
+			//console.log(vgTemp);
 			//VGMFQ
 			for(var j=0; j<vgTemp[1].length;j++)
 			{
@@ -272,18 +285,33 @@ function gotData(data)
 		//console.log(mfq);
 		 
 		
-		/*
+		
 		var responses = users[k].responses;
-			console.log(responses);
+		
+			//console.log(responses);
 			for(var l=0; l < responses.length; l++)
 			{
-				//console.log(responses[l]);
-				var responseInstance = responses[l];
-				responseInstance.
+				//Mass response array
+				
+				/*response[l].characterL/R = Either side
+				  response[l].dilemma = dilemma name and string
+				  response[l].outcome = Death result of outcome.
+				*/
+				responseMass.push(responses[l]);
+				//responseOut.push(responses[l].dilemma);
+				responseOut.push(responses[l].outcome);
+				responseCompare.push(responses[l].characterL);
+				responseCompare.push(responses[l].characterR);
+				responseCompare.push(responses[l].outcome);
+				
+				
+				
+				
+				
 			}
-		*/
+		
 	}
-	//console.log(userChars);
+	
 	//console.log(userGames);
 	
 
@@ -323,8 +351,9 @@ function gotData(data)
 
 	if(!rwmoral)
 	charloaded = true;
-
-console.log(mfq);
+//console.log(responseCompare);
+//console.log(responseOut);
+//console.log(responseMass)
 }
 
 function errData(err) {
@@ -346,7 +375,7 @@ function checkLoaded()
 
 function LoadAge() {
 	checkLoaded();
-	datashow.innerHTML = "</div></br><div>< 20:</div><div> " + less20.length + "</div><br><div>20 - 25:</div><div> " + less26.length + "</div><br><div>26-30:</div><div> " + less30.length + "</div><br><div>30+:</div><div> " + plus30.length + "</div>";
+	datashow.innerHTML = "<div>Age Distribution</div><br><div>< 20:</div><div> " + less20.length + "</div><br><div>20 - 25:</div><div> " + less26.length + "</div><br><div>26-30:</div><div> " + less30.length + "</div><br><div>30+:</div><div> " + plus30.length + "</div>";
 	
 	lastpressed = 0;
 }
@@ -375,7 +404,7 @@ function LoadGenders() {
 			
 	}
 	
-	datashow.innerHTML = "</div></br><div>Male:</div><div> " + maleCount.length + "</div><br><div>Female: </div><div>" + femaleCount.length + "</div><br><div>Other: </div><div>" + otherCount.length + "</div><br><div>N/A:</div><div>" + noCount.length + "</div>";
+	datashow.innerHTML = "<div>Gender Distribution</div><br><div>Male:</div><div> " + maleCount.length + "</div><br><div>Female: </div><div>" + femaleCount.length + "</div><br><div>Other: </div><div>" + otherCount.length + "</div><br><div>N/A:</div><div>" + noCount.length + "</div>";
 	
 	lastpressed = 1;
 }
@@ -445,7 +474,7 @@ function LoadGames() {
 		}
 	}
 	
-	datashow.innerHTML = "</div></br><div>Shooter:</div><div> " + shooterCount.length + "</div><br><div>MOBA:</div><div>" + mobaCount.length + "</div><br><div>RPG:</div><div>" + rpgCount.length + "</div><br><div>Strategy:</div><div> " + strategyCount.length + "</div><br><div>Fighting:</div><div> " + fightingCount.length +
+	datashow.innerHTML = "<div>Game Genres</div><br><div>Shooter:</div><div> " + shooterCount.length + "</div><br><div>MOBA:</div><div>" + mobaCount.length + "</div><br><div>RPG:</div><div>" + rpgCount.length + "</div><br><div>Strategy:</div><div> " + strategyCount.length + "</div><br><div>Fighting:</div><div> " + fightingCount.length +
 	"</div><br><div>Sports: </div><div>" + sportsCount.length + "</div><br><div>Racing: </div><div>" + racingCount.length + "</div><br><div>TCG: </div><div>" + tcgCount.length + "</div><br><div>Survival:</div><div> " + survivalCount.length + "</div><br><div>Puzzle:</div><div> " + puzzleCount.length + "</div><br><div>Other: </div><div>" + otherCount.length + "<br> (" + otherCount + ")</div>";
 	
 	lastpressed = 2;
@@ -552,15 +581,263 @@ function LoadVG(){
 
 
 
+function LoadResponses(){
+	checkLoaded();
+	var responseTemp;
+	var adultDeathCount = 0;
+	var adultnpcDeath = 0;
+	var adulthumanDeath = 0;
+	var kidDeathCount = 0;
+	var kidnpcDeath = 0;
+	var kidhumanDeath = 0;
+
+	var femaCount = 0;
+	var maleCount = 0;
+	var npcCount = 0;
+	var friendCount = 0;
+	var strangerCount = 0;
+	
+	
+	for(var i=0; i<responseOut.length; i++)
+	{
+		responseTemp = responseOut[i];
+		//console.log(responseOut[i]);
+		for(var j=0; j<responseTemp.length; j++)
+		{
+			
+			
+			
+			//console.log(responseTemp[j]);
+			if(responseTemp[j].includes("Female"))
+				femaCount += 1;
+			//console.log("Fema Add: " +responseTemp[j]);
+			else if(responseTemp[j].includes("Male") || responseTemp[j].includes("man"))
+				maleCount +=1;
+			//console.log("Male Add: " +responseTemp[j]);
+			//else //Extra includes Large man, quest failed and "everybody"
+				//console.log("EXTRA   : " + responseTemp[j]);
+			
+			/*
+			if(responseTemp[j].includes("child"))
+				kidCount += 1;
+			else
+				adultCount += 1;
+			*/
+			
+			if(responseTemp[j].includes("NPC"))
+				npcCount += 1;
+			//console.log("NPC Add: " +responseTemp[j]);
+			else if(responseTemp[j].includes("Friend"))
+				friendCount+=1;
+			else if(responseTemp[j].includes("Stranger"))
+				strangerCount+=1;
+			//console.log("Fri Add: " +responseTemp[j]);
+			//else
+				//console.log("EXTRA  : " + responseTemp[j]);
+		}
+	}
+	
+	
+	datashow.innerHTML = "<div>Total Death Counts</div><br><div>Female Death Count:</div><div> " + femaCount + "</div><br><div>Male Death Count:</div><div> " + maleCount + "</div><br><div>NPC Death Count:</div><div>" + npcCount + "</div><br><div>Friend Death Count:</div><div> " + friendCount + "</div><br>";
+	if(rwmoral)
+		datashow.innerHTML += "<div>Stranger Death Count:</div><div> " + strangerCount + "</div><br>";
+	
+	
+	var highestTemp;
+	var mostKilled = 0;
+	var mostSaved = 0;
+	var equalLength = false;
+	var childinleft = false;
+	var childinright = false;
+	var childindilemma = false;
+	//console.log(responseCompare.length);
+	for(var i=0; i<responseCompare.length; i+=3)
+	{
+		
+		if(responseCompare[i].length > responseCompare[i+1].length)
+		{
+			equalLength = false;
+			highestTemp = responseCompare[i].length;
+			//console.log(responseCompare[i].length + ">" + responseCompare[i+1].length);
+		}
+		else if(responseCompare[i+1].length > responseCompare[i].length)
+		{
+			equalLength = false;
+			highestTemp = responseCompare[i+1].length;
+			//console.log(responseCompare[i].length + "<" + responseCompare[i+1].length);
+			//console.log(highestTemp);
+		}
+		else
+		{
+			equalLength = true;
+			//console.log(responseCompare[i].length + "==" + responseCompare[i+1].length);
+			//console.log("Equal length");
+		}
+		
+		if(responseCompare[i+2].length == highestTemp && !equalLength)
+		{
+			//console.log("Result (killed): " + responseCompare[i+2].length + "==" + highestTemp);
+			//console.log("Killed the most");
+			mostKilled += 1;
+		}
+		else if(responseCompare[i+2].length < highestTemp && !equalLength)
+		{
+			//console.log("Result (killed): " + responseCompare[i+2].length + ">" + highestTemp);
+			//console.log("Saved the most");
+			mostSaved += 1;
+		}
+		/*else
+			console.log("Difference is 0");
+			//console.log("Equal Length");
+			*/
+		console.log("///////////////////////////////////");
+		childinleft = false;
+		childinright = false;
+			for(var j=0; j<responseCompare[i].length; j++)
+			{
+				
+				//console.log(responseCompare[i+2][j]);
+				if(responseCompare[i][j].includes("child"))
+				{
+					console.log("Left: " + responseCompare[i][j]);
+					/*for(var k=0; k<responseCompare[i+2].length; k++)
+					{
+						if(!(responseCompare[i+2][k].includes("child")))
+						{
+							//console.log(" Outcome: " + responseCompare[i+2][k]);
+							adultDeathCount += 1;
+						}
+						else
+							kidDeathCount +1 ;
+						*/
+					//}
+					//childindilemma = true;
+					childinleft = true;
+					
+				}
+				else
+					console.log("No child in left (" + responseCompare[i][j] + ")");
+			}
+			
+			
+		if(!childinleft)
+		{
+			for(var j=0; j<responseCompare[i+1].length; j++)
+			{
+				//console.log(responseCompare[i+2][j]);
+				if(responseCompare[i+1][j].includes("child"))
+				{
+					console.log("Right: " + responseCompare[i+1][j]);
+					/*for(var k=0; k<responseCompare[i+2].length; k++)
+					{
+						if(!(responseCompare[i+2][k].includes("child")))
+						{
+							//console.log(" Outcome: " + responseCompare[i+2][k]);
+							adultDeathCount += 1;
+						}
+						else
+							kidDeathCount +1 ;
+					}*/
+					childinright = true;
+					
+				}
+				else
+					console.log("No child in right (" + responseCompare[i+1][j] + ")");
+				
+			}
+		}
+			
+			if(!childinleft && !childinright)
+				console.log("No child in dilemma");
+			else
+			{
+					for(var j=0; j<responseCompare[i+2].length; j++)
+				{
+					if(responseCompare[i+2][j].includes("child"))
+					{
+						console.log("(Kid died) Outcome: " + responseCompare[i+2][j]);
+						kidDeathCount+=1;
+						if(responseCompare[i+2][j].includes("NPC"))
+							kidnpcDeath+=1;
+						else
+							kidhumanDeath+=1;
+						
+					}
+					else
+					{
+						console.log("(Adult died) Outcome: " + responseCompare[i+2][j]);
+						adultDeathCount+=1;
+						if(responseCompare[i+2][j].includes("NPC"))
+							adultnpcDeath+=1;
+						else
+							adulthumanDeath+=1;
+					}
+					
+				}
+			}
+			
+			/*for(var j=0; j<responseCompare[i+1].length; j++)
+			{
+				if(responseCompare[i+1][j].includes("child") && !responseCompare[i+2][j].includes("child"))
+				{
+					console.log(" Right: " + responseCompare[i+1][j] + " Outcome: " + responseCompare[i+2][j]);
+					adultDeathCount += 1;
+				}
+			}
+		*/
+
+		
+		/*
+		for(var j=0; j<responseCompare[i+2].length; j++)
+		{
+			//If the scenario has resulted in a childs death, increase kiddeathcount
+			if(responseCompare[i+2][j].includes("child"))
+			{
+				//console.log(responseCompare[i+2][j]);
+				kidDeathCount += 1;
+			}
+			//Check if the dilemma has a child in i or i+1 but i+2 does not, increase adultdeathcount by 1
+			//else
+				//console.log("NO CHILD:       " + responseCompare[i+2][j]);
+		}
+		*/
+		
+	}
+	
+	datashow.innerHTML += "<div> Age preference* </div><br> <div>Total kid deaths:</div><div>" + kidDeathCount + "</div><br><div>NPC kid deaths:</div><div> " + kidnpcDeath + "</div><br><div>Human kid deaths:</div><div> " + kidhumanDeath + "</div><br><div>Total adult deaths:</div><div> " + adultDeathCount + "</div><br><div>NPC adult deaths:</div><div> " + adultnpcDeath + "</div><br><div>Human adult deaths:</div><div> " + adulthumanDeath + "</div><br>";
+	
+	/*
+	console.log("Total kid deaths: " + kidDeathCount);
+	console.log("Child NPC deaths: " +kidnpcDeath);
+	console.log("Child human deaths: " + kidhumanDeath);
+	
+	console.log("Total adult deaths: " + adultDeathCount);
+	console.log("Adult NPC deaths: " +adultnpcDeath);
+	console.log("Adult human deaths: " + adulthumanDeath);
+	*/
+
+	//console.log("Times highest amount was killed: " + mostKilled);
+	//console.log("Times highest amount was saved: " + mostSaved);
+	
+	datashow.innerHTML += "<br><div>Saving more lives**</div><br><div>More people were saved:</div><div> " + mostSaved + "</div><br><div>More people were killed:</div><div>" + mostKilled + "</div>";
+
+	
+	datashow.innerHTML += "<br><br>* where both adults and children were involved in the dilemma <br><br>** where the dilemma presented options of different death counts"
+	lastpressed = 6;
+	
+	
+}
+
+
 function LoadChar(){
 	checkLoaded();
 	if(!rwmoral)
 	{
 		if(datashow.innerHTML !== "")
-			datashow.innerHTML = "";
+			datashow.innerHTML = "<div>Characters and Games</div><br>";
 		for(var k=0; k<userChars.length-1; k+=2)
 		{
-			datashow.innerHTML += "</div></br><div>Character: </div>      <div>" + userChars[k] + "</div>      <div> Game: </div>      <div>" + userChars[k+1] + "</div><br>";
+			datashow.innerHTML += "<div>" + userChars[k] + "</div> <div>" + userChars[k+1] + "</div><br>";
 		}
 		console.log(userChars);
 	}
